32d898d2f56b48451085bc2fc9669e69b5ac210c
hatobanono <chris00iq@gmail.com>
2022-10-24 18:24:32 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] int1 = new int[] {4, 15};
+			int start = int1[0];
+			int end = int1[1];
+
+			if (start % 2 == 1)
+			{
+				start++;
+			}
+			
+			while (start <= end)
+			{
+				Console.WriteLine(start);
+				start += 2;
+			}
+		}
+	}
+}
